373b7eb71b9e67c64e90d898f2f43f349e5efc88
deepika <43085742+deepikakidwa@users.noreply.github.com>
2018-09-10 10:49:09 +0530

    insert adress for delivery in letsplan

---
+++ insert_address.php
@@ -0,0 +1,13 @@
+		
+	<?php
+		
+		echo $profile=$_POST['profile'];
+		echo $firstname=$_POST['firstname'];
+		echo $location=$_POST['location'];
+		echo $phone=$_POST['phone'];
+		echo $block=$_POST['block'];
+		echo $address=$_POST['address'];
+		echo $lastname=$_POST['lastname'];
+		echo $street=$_POST['street'];
+		echo $avenue=$_POST['avenue'];
+		?>
