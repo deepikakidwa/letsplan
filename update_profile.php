351204189c811f75c20767b8c7ef3a577c975b11
deepika <43085742+deepikakidwa@users.noreply.github.com>
2018-09-10 11:01:25 +0530

    update profile of user in letsplan

---
+++ update_profile.php
@@ -0,0 +1,13 @@
+	<?php 
+	if($_REQUEST){
+		echo"username = ".$username = $_POST['username'];
+		echo"email =".$email = $_POST['email'];
+		echo"moible= ".$mobile = $_POST['mobile'];
+		echo"dob =".$dob = $_POST['dob'];
+		echo "gendr =".$gender = $_POST['gender'];
+		
+		
+	}
+
+
+	?>
