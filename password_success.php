90ccd1ba9e92edae61ba97ba0c63f0f62e803410
deepika <43085742+deepikakidwa@users.noreply.github.com>
2018-09-10 10:55:11 +0530

    password matching sucess file in letsplan

---
+++ password_success.php
@@ -0,0 +1,11 @@
+
+	<?php 
+	if($_REQUEST){
+		echo"newepassword = ".$newepassword = $_POST['newepassword'];
+		echo"confirmpassword =".$confirmpassword = $_POST['confirmpassword'];
+		
+		
+	}
+
+
+	?>
